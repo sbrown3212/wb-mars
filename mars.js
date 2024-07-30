062a59d35c45d9fe7a4ec63d43d3aac8e883069a
Stephen Brown <stephen.jr36@gmail.com>
2024-07-30 13:50:50 -0600

    completed

---
+++ mars.js
@@ -0,0 +1,19 @@
+// alert(`Starting you Mars Adventure!`);
+// alert(`Booting up...`);
+// alert(`All systems go!`);
+// alert(`Let's Start`);
+
+// const username = prompt(`Hi there. What's your name?`);
+
+// alert(`Hi ${username} --- Welcome to The Mars Adventrue Game.`);
+// alert(`Yesterday, you received a call from your good friend at NASA,`);
+// alert(`They ned someone to go to Mars this weekend, and YOU'VE been choosen!!`);
+
+const userInput = prompt(`Are you excited? (Type Y or N)`);
+
+if (userInput === `Y`) {
+    alert(`I knew you'd say that. It's so cool that you're going to Mars!`);
+} else if (userInput === `N`) {
+    alert(`Well, it's too late to back out now.`);
+}
+
