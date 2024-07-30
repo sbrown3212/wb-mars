6df8c6b5bed2f1620f04ed8e84869df965017638
Stephen Brown <stephen.jr36@gmail.com>
2024-07-30 14:17:41 -0600

    completed improving companion animal

--- mars.js
+++ mars.js
@@ -9,30 +9,44 @@
 // alert(`Yesterday, you received a call from your good friend at NASA,`);
 // alert(`They ned someone to go to Mars this weekend, and YOU'VE been choosen!!`);
 
-let excited = prompt(`Are you excited? (Type Y or N)`);
+// let excited = prompt(`Are you excited? (Type Y or N)`);
 
-excited = excited.toUpperCase();
+// excited = excited.toUpperCase();
 
-if (excited === `Y`) {
-    alert(`I knew you'd say that. It's so cool that you're going to Mars!`);
-} else if (excited === `N`) {
-    alert(`Well, it's too late to back out now.`);
-}
+// if (excited === `Y`) {
+//     alert(`I knew you'd say that. It's so cool that you're going to Mars!`);
+// } else if (excited === `N`) {
+//     alert(`Well, it's too late to back out now.`);
+// }
 
-alert(`It's time to pack for your trip to Mars.`);
-let numSuitcases = prompt(`How many suitcases do you plan to bring?`);
+// alert(`It's time to pack for your trip to Mars.`);
+// let numSuitcases = prompt(`How many suitcases do you plan to bring?`);
 
-numSuitcases = Number(numSuitcases);
+// numSuitcases = Number(numSuitcases);
 
-if (numSuitcases > 2) {
-    alert(`That's way too many. You'll have to pack more lightly.`);
-    alert(`Please try to fit your stuff into 2 suitcases.`);
-} else {
-    alert(`Perfect. You'll certainly fit in the spaceship!`);
-}
+// if (numSuitcases > 2) {
+//     alert(`That's way too many. You'll have to pack more lightly.`);
+//     alert(`Please try to fit your stuff into 2 suitcases.`);
+// } else {
+//     alert(`Perfect. You'll certainly fit in the spaceship!`);
+// }
 
 alert(`You're allowed to bring one companion animal with you.`);
-const companionType = prompt(`What kind of companion animal would you like to bring?`);
-const companionName = prompt(`What is your companion's name?`);
+
+let companionType = prompt(`What kind of companion animal would you like to bring?`);
+let companionName = prompt(`What is your companion's name?`);
+
+let firstLetter = companionName[0];
+firstLetter = firstLetter.toUpperCase();
+let otherLetters = companionName.slice(1);
+otherLetters = otherLetters.toLocaleLowerCase();
+companionName = firstLetter + otherLetters;
+
+firstLetter = companionType[0];
+firstLetter = firstLetter.toUpperCase();
+otherLetters = companionType.slice(1);
+otherLetters = otherLetters.toLocaleLowerCase();
+companionType = firstLetter + otherLetters;
+
 alert(`Cool, so you're bringing ${companionName} the ${companionType}.`);
 
