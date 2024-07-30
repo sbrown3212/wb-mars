ad6690a15873889b09576f3876591ba0bfc057bd
Stephen Brown <stephen.jr36@gmail.com>
2024-07-30 15:33:08 -0600

    completed harder features

--- mars.js
+++ mars.js
@@ -5,9 +5,31 @@ alert(`Let's Start`);
 
 const username = prompt(`Hi there. What's your name?`);
 
-alert(`Hi ${username} --- Welcome to The Mars Adventrue Game.`);
-alert(`Yesterday, you received a call from your good friend at NASA,`);
-alert(`They ned someone to go to Mars this weekend, and YOU'VE been choosen!!`);
+// alert(`Hi ${username} --- Welcome to The Mars Adventrue Game.`);
+// alert(`Yesterday, you received a call from your good friend at NASA,`);
+// alert(`They need someone to go to Mars this weekend, and YOU'VE been choosen!!`);
+
+alert(`Hi ${username} --- Welcome to the Space Adventure Game.`);
+
+let planet = prompt(`What planet would you like to travel too?`).trim();
+
+let firstLetter = planet[0];
+firstLetter = firstLetter.toUpperCase();
+let otherLetters = planet.slice(1);
+otherLetters = otherLetters.toLowerCase();
+planet = firstLetter + otherLetters;
+
+if (planet === `Saturn`) {
+    alert(`The spaceship for Saturn left last week`);
+} else if (planet === `Venus`) {
+    alert(`It's not worth the trip.`);
+} else if (planet === `Mercury`) {
+    alert(`It's the wrong season for Mercury.`)
+} else {
+    alert(`Mars is far more interesting.`)
+}
+
+alert(`Looks like you're going to Mars.`)
 
 let excited = prompt(`Are you excited? (Type Y or N)`);
 
@@ -37,9 +59,9 @@ alert(`You're allowed to bring one companion animal with you.`);
 let companionType = prompt(`What kind of companion animal would you like to bring?`);
 let companionName = prompt(`What is your companion's name?`);
 
-let firstLetter = companionName[0];
+firstLetter = companionName[0];
 firstLetter = firstLetter.toUpperCase();
-let otherLetters = companionName.slice(1);
+otherLetters = companionName.slice(1);
 otherLetters = otherLetters.toLocaleLowerCase();
 companionName = firstLetter + otherLetters;
 
@@ -82,7 +104,7 @@ secondCompanionName = firstLetter + otherLetters;
 alert(`So ${secondCompanionName} the ${secondCompanionType} is coming too.`);
 alert(`But ${secondCompanionName} will probably need to ride in the cargo bay so that they don't get the ${decor} decor messy.`)
 
-alert(`${username} and ${companionName}, surfing the celestial abyss, in a ${decor} spaceship, With ${secondCompanionName} in the cargo bay.`);
+alert(`${username} and ${companionName}, surfing the celestial abyss, in a ${decor} spaceship, with ${secondCompanionName} in the cargo bay.`);
 
 let timer = 5;
 while (timer > 0) {
